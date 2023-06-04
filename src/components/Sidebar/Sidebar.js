e4213c249486bcc1fb587242be32773cd0d4ad25
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-06-04 13:29:00 +0530

    changes-sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,32 +1,36 @@
-import React from 'react'
-import './Sidebar.css'
-import { auth } from '../../firebase';
-import { useNavigate } from 'react-router';
-import { signOut } from '@firebase/auth';
+import React from "react";
+import "./Sidebar.css";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router";
+import { signOut } from "@firebase/auth";
 
-const Sidebar = ({isAuth, setIsAuth}) => {
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        // Handle logout logic here
-        signOut(auth).then(() => {
-            localStorage.clear();
-            setIsAuth(false);
-            navigate("/login");
-        });
-    };
+const Sidebar = ({ isAuth, setIsAuth }) => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    // Handle logout logic here
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login");
+    });
+  };
   return (
-    <div className='sidebar-container'>
-        <h1>Finance Tracker</h1>
-        <ul>
-            {isAuth && <li onClick={() => navigate('/transactions')}>Transactions</li>}
-            {isAuth && <li onClick={() => navigate('/add-expense')}>Add Expenses</li>}
-            {isAuth && <li onClick={() => navigate('/add-income')}>Add Incomes</li>}
-            {isAuth && <li onClick={() => navigate('/stats')}>Stats</li>}
-            {!isAuth && <li onClick={() => navigate('/login')}>Login</li>}
-            {isAuth && <li onClick={handleLogout}>Logout</li>}
-        </ul>
+    <div className="sidebar-container">
+      <h1>Finance Tracker</h1>
+      <ul>
+        {isAuth && (
+          <li onClick={() => navigate("/transactions")}>Transactions</li>
+        )}
+        {isAuth && (
+          <li onClick={() => navigate("/add-expense")}>Add Expenses</li>
+        )}
+        {isAuth && <li onClick={() => navigate("/add-income")}>Add Incomes</li>}
+        {isAuth && <li onClick={() => navigate("/stats")}>Stats</li>}
+        {!isAuth && <li onClick={() => navigate("/login")}>Login</li>}
+        {isAuth && <li onClick={handleLogout}>Logout</li>}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
